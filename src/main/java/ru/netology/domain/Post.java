6845e1cb38a39d583209a37ef0de4b9233e44d58
Daria Lukyanova <luk.dyr@yandex.ru>
2021-10-18 15:34:37 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int text;
+    private int views;
+    private int additionally;
+    private int picture;
+    private int likes;
+    private int comments;
+    private int reposts;
+}
